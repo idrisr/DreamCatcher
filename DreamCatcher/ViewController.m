2e9323c223d789417afa70388408ee627cc09a87
Idris Raja <idris.raja@gmail.com>
2016-03-03 00:00:29 -0600

    display a list. some odd UICollectionViewFlowLayoutBreakForInvalidSizes exception

--- DreamCatcher/ViewController.m
+++ DreamCatcher/ViewController.m
@@ -8,31 +8,64 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property NSMutableArray *dreamList;
 @end
 
 @implementation ViewController
 
 - (void) viewDidLoad {
     [super viewDidLoad];
+    self.dreamList = [NSMutableArray new];
 }
 
 - (IBAction) onAddButtonPressed:(UIBarButtonItem *) sender {
+    [self presentDreamEntry];
 }
 
 - (IBAction) onEditButtonPressed:(UIBarButtonItem *) sender {
 }
 
+- (void) presentDreamEntry {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Add new dream"
+                                                                             message:nil
+                                                                      preferredStyle:UIAlertControllerStyleAlert];
+
+    [alertController addTextFieldWithConfigurationHandler: ^(UITextField *_Nonnull textField) {
+        textField.placeholder = @"Dream Title";
+    }];
+
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                           style:UIAlertActionStyleCancel
+                                                         handler:nil];
+
+    UIAlertAction *saveAction = [UIAlertAction actionWithTitle:@"Save"
+                                                         style:UIAlertActionStyleDefault
+                                                       handler: ^(UIAlertAction *_Nonnull action) {
+                                                           UITextField *textField = alertController.textFields.firstObject;
+                                                           [self.dreamList addObject:textField.text];
+                                                           [self.tableView reloadData];
+                                                       }];
+
+    [alertController addAction: cancelAction];
+    [alertController addAction: saveAction];
+
+    [self presentViewController:alertController animated:YES completion:nil];
+
+}
+
 - (NSInteger)   tableView:(UITableView *) tableView
     numberOfRowsInSection:(NSInteger) section {
-    return 1;
+    return [self.dreamList count];
 }
 
 - (UITableViewCell *) tableView:(UITableView *) tableView
           cellForRowAtIndexPath:(NSIndexPath *) indexPath {
     NSString *reuseIdentifier = @"cellID";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifier];
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:reuseIdentifier];
+
+    cell.textLabel.text = self.dreamList[indexPath.row];
     return cell;
 }
 
